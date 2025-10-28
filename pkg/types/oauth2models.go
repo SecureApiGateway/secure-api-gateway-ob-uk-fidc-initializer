2976f4d68c83183b6e725f2688b26c21f30de0a4
Shaun Harrison <shaun.harrison@pingidentity.com>
2025-10-28 12:51:27 +0000

    OpenIG-9761: Change requestObjectProcessing to JAR

--- pkg/types/oauth2models.go
+++ pkg/types/oauth2models.go
@@ -57,6 +57,7 @@ type (
 		TokenCompressionEnabled                    bool          `json:"tokenCompressionEnabled"`
 		AllowedAudienceValues                      []interface{} `json:"allowedAudienceValues"`
 		TLSCertificateRevocationCheckingEnabled    bool          `json:"tlsCertificateRevocationCheckingEnabled"`
+		RequestObjectProcessing                    []string      `json:"requestObjectProcessing"`
 	}
 
 	CoreOIDCConfig struct {
